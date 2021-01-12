c0ef7ebf7cc348b81ca9f2ed87989c6dd433e39e
Alexey Shvetsov <shvet12sov@gmail.com>
2021-01-12 14:58:05 +0300

    hotfix: второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,40 @@
 let popup = document.querySelector('.popup');//
 let editButton = document.querySelector('.profile__button-edit');//
 let closeProfileButton = popup.querySelector('.popup__close');//
-let noScroll = document.querySelector('.body'); //
 let popupForm = popup.querySelector('.popup__container');
-let nameInput = popupForm.querySelector('.popup__name');//
-let descInput = popupForm.querySelector('.popup__description');//
+let nameInput = popupForm.querySelector('.popup__input_name');//
+let descInput = popupForm.querySelector('.popup__input_description');//
 let profileName = document.querySelector('.profile__name');//
 let profileDesc = document.querySelector('.profile__description');//
 
-let togglePopup = () => {
-  popup.classList.toggle('popup_opened')
-  noScroll.classList.toggle('body_noscroll')
-  console.log('popup')
+let popupOpen = () => {
+  popup.classList.add('popup_opened');
+  profileInfoInput();
+}
+
+let popupClose = () => {
+  popup.classList.remove('popup_opened')
 }
 
 let profileInfoInput = () => {
-  nameInput.value = profileName.textContent
-  descInput.value = profileDesc.textContent
-  console.log('copy in input')
+  nameInput.value = profileName.textContent;
+  descInput.value = profileDesc.textContent;
 }
 
 let handleSumbitForm = (evt) => {
   evt.preventDefault();
-  profileName.textContent = nameInput.value
-  profileDesc.textContent = descInput.value
-  togglePopup();
+  profileName.textContent = nameInput.value;
+  profileDesc.textContent = descInput.value;
+  popupClose();
 }
 
-editButton.addEventListener('click', togglePopup)
-editButton.addEventListener('click', profileInfoInput)
-closeProfileButton.addEventListener('click', togglePopup)
-popupForm.addEventListener('submit', handleSumbitForm)
+editButton.addEventListener('click', popupOpen);
+closeProfileButton.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', handleSumbitForm);
 
 popup.addEventListener('click', (event) => {
   if (event.target === event.currentTarget) {
-    togglePopup();
+    popupClose();
   }
 })
 
